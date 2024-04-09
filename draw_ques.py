6a5a515a29ef3d75d045b5de1ed5e4df94376062
steve <stevemkf@gmail.com>
2024-04-09 20:54:12 +0800

    Login page added. Candidate results saved in database.

--- draw_ques.py
+++ draw_ques.py
@@ -2,61 +2,73 @@ import pandas as pd
 import openpyxl
 import random
 
-class DrawQuestions():
 
+# Parse the question bank Excel file and build up a 2D question position list, based on Group and Category
+class DrawQuestions():
     def __init__(self):
+        self.df = pd.read_excel("questions.xlsx", sheet_name="TTR")
+
         # Question group: M, N, O, P
         # Question category: A to H
         total_groups = 4
         total_cats = 8
         # Create a 2D question position list.  Position means Excel dataframe row number
-        question_pos_lists = [[[] for col in range(total_cats)] for row in range(total_groups)]
+        self.question_pos_lists = [[[] for col in range(total_cats)] for row in range(total_groups)]
 
         # Parse all rows in the question worksheet and fill in the 2D question position lists
-        self.df = pd.read_excel("questions.xlsx", sheet_name="TTR")
-        for index, row in self.df.iterrows():
+        for index_df, row in self.df.iterrows():
             # question_num: e.g. M01A, P04H
             question_num = row['no']
             group = ord(question_num[0]) - ord('M')
             cat = ord(question_num[len(question_num) - 1]) - ord('A')
-            question_pos_lists[group][cat].append(index)
+            self.question_pos_lists[group][cat].append(index_df)
 
+
+    # Randomly draw questions for one test paper.  Return a list of indexes for the dataframe
+    def get_ques_list(self):
         # Number of questions to be drawn from each category
-        total_ques_cat = [10, 10, 10, 10, 15, 15, 15, 15]
+        ques_per_cat = [10, 10, 10, 10, 15, 15, 15, 15]
 
         # Draw questions for a test paper
-        self.ques_pos_paper = []
+        ques_list = []
         # The questions will follow the category orders, i.e. A to H
-        for index, num_ques_cat in enumerate(total_ques_cat):
+        for index_cat, num_ques_cat in enumerate(ques_per_cat):
             # Choose either Group M or N + either Group O or P for each category of questions
             group1 = random.randint(0, 1)
             group2 = random.randint(2, 3)
-            cat_ques_pos = question_pos_lists[group1][index] + question_pos_lists[group2][index]
-            ques_pos_drawn = random.sample(cat_ques_pos, num_ques_cat)
+            cat_ques_list = self.question_pos_lists[group1][index_cat] + self.question_pos_lists[group2][index_cat]
+            cat_ques_drawn = random.sample(cat_ques_list, num_ques_cat)
             # Sort the questions so that it is easier to check the results
-            ques_pos_drawn.sort()
+            cat_ques_drawn.sort()
             # Combine the questions to form the test paper
-            self.ques_pos_paper = self.ques_pos_paper + ques_pos_drawn
+            ques_list = ques_list + cat_ques_drawn
+        return ques_list
 
-        self.total_ques_paper = len(self.ques_pos_paper)
+
+    def get_ques_num_ans_list(self, ques_list):
+        ques_num_list = []
+        ques_ans_list = []
+        for index_df in ques_list:
+            row_content = self.df.iloc[index_df]
+            ques_num_list.append(row_content['no'])
+            ques_ans_list.append(str(row_content['ans']))
+        return ques_num_list, ques_ans_list
 
 
-    # Retrieve the question contents from dataframe, based on their position numbers
-    def get_question(self, index):
-        index_df = self.ques_pos_paper[index]
+    # Return full data of a particular question in the format of Python dictionary
+    def get_question(self, index_df):
         row_content = self.df.iloc[index_df]
-        d = dict()
-        d["question_num"] = row_content['no']    # e.g. e.g. M01A, P04H
-        d["question"] = row_content['question']
-        d["choice_1"] = str(row_content['cho1'])
-        d["choice_2"] = str(row_content['cho2'])
-        d["choice_3"] = str(row_content['cho3'])
-        d["choice_4"] = str(row_content['cho4'])
-        d["answer"] = row_content['ans']
+        ques = dict()
+        ques["question_num"] = row_content['no']    # e.g. e.g. M01A, P04H
+        ques["question"] = row_content['question']
+        ques["choice_1"] = str(row_content['cho1'])
+        ques["choice_2"] = str(row_content['cho2'])
+        ques["choice_3"] = str(row_content['cho3'])
+        ques["choice_4"] = str(row_content['cho4'])
+        ques["answer"] = row_content['ans']
         image = row_content['pic']
         # empty cell return NaN which should be replaced with empty string
         if image != image:
             image = ""
-        d["image"] = image
-        return(d)
-
+        ques["image"] = image
+        return ques
--- main.py
+++ main.py
@@ -1,57 +1,139 @@
 from flask import Flask, render_template, request, redirect, session
-from flask_session import Session
 from draw_ques import DrawQuestions
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, date
+
 
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-q = DrawQuestions()
-ans_list = ['0'] * q.total_ques_paper
+app.config["SECRET_KEY"] = "there_is_no_secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)                        # Candidates' data will be saved onto database
+q = DrawQuestions()                         # Carry out the preparation.  Questions will be drawn later.
+
+
+# define the structure of database table
+class Candidate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    candidate_no = db.Column(db.String(20))
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(20))
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    ques_num_list = db.Column(db.String(1000))
+    correct_ans_list = db.Column(db.String(400))
+    ans_list = db.Column(db.String(400))
+    final_score = db.Column(db.Integer)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    increment = 0                               # Python interpreter insisted adding this line
-
     if request.method == "POST":
-        direction = request.form["direction"]
-        if direction == "next":
-            increment = 1
-        elif direction == "prev":
-            increment = -1
+        session['candidate_no'] = request.form["candidate_no"]
+        ques_list = q.get_ques_list()                           # draw one set of questions for each candidate
+        session['ques_list'] = ques_list
+        session['ques_num_list'], session['correct_ans_list'] = q.get_ques_num_ans_list(ques_list)
+        session['total_ques'] = len(session['ques_list'])
+        session['ans_list'] = ['0'] * session['total_ques']     # each candidate has his/her answer list as well
+        session['ques_no'] = 1
 
+        today = date.today()
+        t_now = datetime.now().time()
+
+        # convert Python list to delimited string or it could not be stored into database
+        ques_num_list = ",".join(item for item in session['ques_num_list'])
+        correct_ans_list = ",".join(item for item in session['correct_ans_list'])
+        candidate = Candidate(candidate_no=session['candidate_no'], date=today, time=t_now, ques_num_list=ques_num_list,
+                              correct_ans_list=correct_ans_list)
+        db.session.add(candidate)
+        db.session.commit()
+
+        session.modified = True
+        return redirect("/test")
+
+    return render_template("index.html")
+
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+
+    # candidate must log in first
+    if not 'candidate_no' in session.keys():
+        return redirect("/index")
+
+
+    increment = 0  # Python interpreter insists declare the variable first
+    if request.method == "POST":
+        # save the candidate's chosen answer
         if "answer" in request.form:
             answer = request.form["answer"]
-            ans_list[session['ques_no'] - 1] = answer
+            session['ans_list'][session['ques_no'] - 1] = answer
 
-    if not session.get("ques_no"):
-        session['ques_no'] = 1
-    else:
-        session['ques_no'] = session['ques_no'] + increment
+        # end of the test?
+        if "finish" in request.form:
+            # without the following line, the answer picked just before finish would not be passed by session variable
+            session.modified = True
+            return redirect("/finish")
 
-    if session['ques_no'] > q.total_ques_paper:
-        session['ques_no'] = 1
-    if session['ques_no'] == 0:
-        session['ques_no'] = q.total_ques_paper
+        # the candidate wants to move the next or previous question?
+        if "direction" in request.form:
+            direction = request.form["direction"]
+            if direction == "next":
+                increment = 1
+            elif direction == "prev":
+                increment = -1
 
-    d = q.get_question(session['ques_no'] - 1)
 
-    return render_template("index.html",
+    ques_no = session['ques_no'] + increment
+    if ques_no > session['total_ques']:
+        ques_no = 1
+    if ques_no == 0:
+        ques_no = session['total_ques']
+    session['ques_no'] = ques_no
+
+    # prepare the next/previous question
+    index_df = session['ques_list'][ques_no - 1]
+    ques = q.get_question(index_df)
+    exist_ans = session['ans_list'][ques_no - 1]
+
+    # show the question and answers to the candidate through HTML
+    return render_template("test.html",
                            ques_num=session['ques_no'],
-                           question=d["question"],
-                           choice_1=d["choice_1"],
-                           choice_2=d["choice_2"],
-                           choice_3=d["choice_3"],
-                           choice_4=d["choice_4"],
-                           fname_image=d["image"])
+                           question=ques["question"],
+                           choice_1=ques["choice_1"],
+                           choice_2=ques["choice_2"],
+                           choice_3=ques["choice_3"],
+                           choice_4=ques["choice_4"],
+                           fname_image=ques["image"],
+                           exist_ans=exist_ans)
 
-@app.route("/logout")
-def logout():
-    session["ques_no"] = None
-    return redirect("/")
+
+@app.route("/finish")
+def finish():
+    # candidate's answers will be stored in database
+    candidate_no = session["candidate_no"]
+    ans_list = session['ans_list']
+    ans_list_str = ",".join(item for item in ans_list)
+    print(ans_list)
+
+    # compare candidate's answers to the correct answers, hence calculate the final score
+    candidate = Candidate.query.filter_by(candidate_no=candidate_no).first()
+    correct_ans_list = candidate.correct_ans_list.split(",")
+    final_score = 0
+    for index in range(0, session['total_ques']):
+        if ans_list[index] == correct_ans_list[index]:
+            final_score = final_score + 1
+
+    # save candidate's answers and final score to database
+    candidate.ans_list = ans_list_str
+    candidate.final_score = final_score
+    db.session.commit()
+
+    # clear the Session variables
+    session.clear()
+    return "<p>Test finished!</p>"
 
 
 if __name__ == "__main__":
     with app.app_context():
-        app.run(debug=True, port=5001)
+        db.create_all()
+        app.run(debug=True, host='192.168.1.69', port=5003)
